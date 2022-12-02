d0289a7e1ebd824b93fda55342ebb1fcf342b98b
Arthur Simas <arthursimas1@gmail.com>
2022-12-02 08:30:34 -0300

    reading vsys metric

--- src/sys_stats.py
+++ src/sys_stats.py
@@ -1,17 +1,22 @@
-import machine
+from machine import ADC, Pin
 
 
 class SYSStats:
     def __init__(self):
-        self.internal_temp_adc = machine.ADC(4)
+        self.internal_temp_adc = ADC(4)
+        self.internal_voltage_adc = ADC(Pin(29))
 
     def read(self):
         conversion_factor = 3.3 / 65535
-        temp_reading = self.internal_temp_adc.read_u16() * conversion_factor 
-        temp = 27 - (temp_reading - 0.706) / 0.001721
+
+        temp_reading = self.internal_temp_adc.read_u16()
+        temp = 27 - (temp_reading * conversion_factor - 0.706) / 0.001721
+
+        vsys_reading = self.internal_temp_adc.read_u16()
+        vsys = vsys_reading * conversion_factor
 
         return {
             #'status': status,
             'cpu/temperature': { 'raw' : { 'adc': temp_reading }, 'value': temp, 'unit': 'ºC' },
-            #'power/voltage': { 'raw' : { 'adc': volt_reading }, 'value': volt, 'unit': 'V' },
+            'power/vsys': { 'raw' : { 'adc': vsys_reading }, 'value': vsys, 'unit': 'V' },
         }
