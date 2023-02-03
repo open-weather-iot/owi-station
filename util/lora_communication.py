c9c5a6cd00f72f25104eaae600c4a71e506a4df3
Arthur Simas <arthursimas1@gmail.com>
2023-02-03 20:49:45 -0300

    improving constants readability

--- util/lora_communication.py
+++ util/lora_communication.py
@@ -7,8 +7,11 @@ from lib.ulora import TTN, uLoRa
 _LORA_IRQ = const(20) # DIO0
 _LORA_RST = const(21) # RESET
 
-_MAX_PAYLOAD_SIZE = 241 # 242 bytes payload size limit - 1 byte header
-_MAX_FRAGMENT_PAYLOAD_SIZE = 228 # 242 bytes payload size limit - 1 byte header - 10 bytes packet id - 3 bytes sequence number
+_PACKET_TYPE_SIZE = 1
+_PACKET_ID_SIZE = 10
+_SEQUENCE_SIZE = 3
+_MAX_PAYLOAD_SIZE = 242 - _PACKET_TYPE_SIZE
+_MAX_FRAGMENT_PAYLOAD_SIZE = 242 - _PACKET_TYPE_SIZE - _PACKET_ID_SIZE - _SEQUENCE_SIZE
 _PKT_TYPE_NOT_FRAG = b'0'
 _PKT_TYPE_FRAG_PARTIAL = b'1'
 _PKT_TYPE_FRAG_FINAL = b'2'
@@ -21,20 +24,22 @@ class LoRaCommunication:
             irq=_LORA_IRQ,
             rst=_LORA_RST,
             ttn_config=TTN(device_addr, network_key, app_key, country='AU'), # defines the frequency plan (brazil uses the australian standard AU915-928)
-            datarate='SF7BW125', # spreading factor - SR: 7; band width - BW: 125
+            datarate='SF7BW125', # spreading factor - SR; band width - BW
             fport=1,
         )
 
     def _send(self, data):
-        self.lora.send_data(data, len(data), self.lora.frame_counter)
+        length = len(data)
         self.lora.frame_counter += 1
+        self.lora.send_data(data, length, self.lora.frame_counter)
+        return length
 
     def send(self, data):
         data = json.dumps(data, separators=(',', ':')).encode('utf8')
-        if len(data) <= _MAX_PAYLOAD_SIZE:
+        length = len(data)
+        if length <= _MAX_PAYLOAD_SIZE:
             return self._send(_PKT_TYPE_NOT_FRAG + data)
 
-        # TYPE (1) + PACKET ID (10) + SEQUENCE NUMBER (3)
         pkt_id = random.getrandbits(32)
         sequence = 0
 
@@ -51,3 +56,5 @@ class LoRaCommunication:
             time.sleep_ms(200)
 
             sequence += 1
+
+        return length
